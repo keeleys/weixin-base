4ea3e636bdd08bdd0f3511f127a455487d9b84e4
keeley <keeley.jun@qq.com>
2016-01-21 10:48:12 +0800

    add

--- weixin-service/src/main/java/com/ttianjun/ctrl/WeixinController.java
+++ weixin-service/src/main/java/com/ttianjun/ctrl/WeixinController.java
@@ -5,6 +5,7 @@ import com.github.sd4324530.fastweixin.message.BaseMsg;
 import com.github.sd4324530.fastweixin.message.TextMsg;
 import com.github.sd4324530.fastweixin.message.req.BaseReqMsg;
 import com.github.sd4324530.fastweixin.servlet.WeixinControllerSupport;
+import com.ttianjun.handle.BaseMsgHandle;
 import com.ttianjun.handle.TextMsgHandle;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -45,5 +46,15 @@ public class WeixinController extends WeixinControllerSupport {
     }
 
 
-
+    /*1.1版本新增，重写父类方法，加入自定义微信消息处理器
+     *不是必须的，上面的方法是统一处理所有的文本消息，如果业务觉复杂，上面的会显得比较乱
+     *这个机制就是为了应对这种情况，每个MessageHandle就是一个业务，只处理指定的那部分消息
+     */
+    @Override
+    protected List<MessageHandle> initMessageHandles() {
+        List<MessageHandle> handles = new ArrayList<MessageHandle>();
+        handles.add(new TextMsgHandle());
+        handles.add(new BaseMsgHandle());
+        return handles;
+    }
 }
